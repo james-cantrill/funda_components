d3ae4d56298039a5a8b28122a0b3c0dc3fa26888
James E. Cantrill <jcantril@verizon.net>
2016-10-31 11:37:42 -0400

    Create system_actions.sql

---
+++ system_user/system_user_database/table_definitions/system_actions.sql
@@ -0,0 +1,16 @@
+
+DROP TABLE IF EXISTS system_user_schema.system_actions;
+
+CREATE TABLE system_user_schema.system_actions (
+	service	text NOT NULL,
+	action	text NOT NULL,
+	action_display_name	text,
+	UNIQUE (service, action)
+);
+
+-- insert the actions for the system_user service that require authorization (login and logout don't). The other services will insert their actions as part of their installation
+INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('system_user', 'add_new_user', 'Add User');
+INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('system_user', 'enter_edit_allowed_actions', 'Allowed Actions');
+INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('system_user', 'change_password', 'Change Password');
+
+
