28cb94c0b95cda74953b6a544d150591eb042252
James E. Cantrill <jcantril@verizon.net>
2016-10-31 11:59:59 -0400

    Add files via upload

---
+++ programs_manager/programs_manager_database/install_programs_manager_schema_and_objects.bat
@@ -0,0 +1,42 @@
+REM install_programs_manager_schema_and_objects.bat
+
+REM install the schema
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\create_programs_manager_schema.sql  1> install_programs_manager_schema_and_objects.log 2>&1 
+
+REM -- insert the actions for the programs_manager service that require authorization.
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\table_definitions\insert_system_actions_for_prgrm_mngr.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+REM install the objects
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\table_definitions\continua.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\table_definitions\organizations.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\table_definitions\programs.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\table_definitions\system_user_allowed_programs.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\table_definitions\system_user_allowed_programs_history.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\table_definitions\system_user_allowed_organizations.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\table_definitions\system_user_allowed_organizations_history.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\table_definitions\insert_continua.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\table_definitions\insert_organizations.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\table_definitions\insert_programs.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\action_functions\action_change_program_user_visibility.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\action_functions\action_list_user_visible_organizations.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\action_functions\action_list_user_visible_programs.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\action_functions\action_change_organization_user_visibility.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\utility_functions\trig_log_user_allowed_programs_history.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\utility_functions\insert_master_user_prgrm_mngr_actions.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\programs_manager\programs_manager_database\utility_functions\run_insert_master_user_prgrm_mngr_actions.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
