47e63a22773c0af4ee72559617951d8be3b7c588
James E. Cantrill <jcantril@verizon.net>
2016-10-31 11:41:12 -0400

    Create test_util_get_user_state.sql

---
+++ system_user/system_user_database/utility_functions/test/test_util_get_user_state.sql
@@ -0,0 +1,13 @@
+-- test_system_user_schema.util_get_user_state
+
+SELECT * FROM system_user_schema.util_get_user_state ('opal');
+
+SELECT * FROM system_user_schema.util_get_user_state ('mkicks');
+
+SELECT * FROM system_user_schema.util_get_user_state ('goper');
+
+SELECT * FROM system_user_schema.util_get_user_state ('gopal');
+
+SELECT * FROM system_user_schema.util_get_user_state ('hmiller');
+
+SELECT * FROM system_user_schema.util_get_user_state ('pbetz');
