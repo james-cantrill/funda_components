e2377f529319de380a2fb0495152704404b5fbbb
Jim Cantrill <CantriJ@DB0NYY1.ccst.ccdor.local>
2016-12-13 15:53:01 -0500

    add initiation objects

--- programs_manager/programs_manager_database/action_functions/test/test_action_change_program_user_visibility.bat
+++ programs_manager/programs_manager_database/action_functions/test/test_action_change_program_user_visibility.bat
@@ -1,6 +1,8 @@
 
 "C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f ..\action_change_program_user_visibility.sql 1> test_action_change_program_user_visibility.log 2>&1 
 
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\programs_manager\programs_manager_database\table_definitions\test\test_insert_organizations_and_programs.sql  1>> test_action_change_program_user_visibility.log 2>>&1 
+
 "C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f test_action_change_program_user_visibility.sql  1>> test_action_change_program_user_visibility.log 2>>&1 
 
  
--- programs_manager/programs_manager_database/action_functions/test/test_action_list_user_visible_programs.bat
+++ programs_manager/programs_manager_database/action_functions/test/test_action_list_user_visible_programs.bat
@@ -1,6 +1,8 @@
 
 "C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f ..\action_list_user_visible_programs.sql 1> test_action_list_user_visible_programs.log 2>&1 
 
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\programs_manager\programs_manager_database\table_definitions\test\test_insert_organizations_and_programs.sql  1>> test_action_list_user_visible_programs.log 2>>&1 
+
 "C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f test_action_list_user_visible_programs.sql  1>> test_action_list_user_visible_programs.log 2>>&1 
 
  
