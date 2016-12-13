41525d7fa1f4c2c0826e217096b7b22dfcbf861a
Jim Cantrill <CantriJ@DB0NYY1.ccst.ccdor.local>
2016-12-13 15:55:46 -0500

    add new objects and initial data

--- programs_manager/programs_manager_database/table_definitions/programs.sql
+++ programs_manager/programs_manager_database/table_definitions/programs.sql
@@ -6,15 +6,16 @@ CREATE TABLE programs_manager_schema.programs (
 	program_name	text,
 	organization_id	text,
 	coc_code	text  NOT NULL,
+	containing_folder_name	text NOT NULL,
 	datetime_program_changed	timestamp without time zone NOT NULL,
 	changed_by_user_login	text NOT NULL,
     CONSTRAINT program_name_duplicate UNIQUE(program_name),
 	CONSTRAINT programs_organization_id_fkey FOREIGN KEY (organization_id)
 		REFERENCES programs_manager_schema.organizations (organization_id) MATCH SIMPLE
 		ON UPDATE NO ACTION ON DELETE CASCADE,
-	CONSTRAINT programs_coc_code_fkey FOREIGN KEY (coc_code)
-		REFERENCES programs_manager_schema.continua (coc_code) MATCH SIMPLE
-		ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT programs_containing_folder_name_fkey FOREIGN KEY (containing_folder_name)
+		REFERENCES programs_manager_schema.program_folders (program_folder_name) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE CASCADE,
 	CONSTRAINT programs_changed_by_user_login_fkey FOREIGN KEY (changed_by_user_login)
 		REFERENCES system_user_schema.system_users (login) MATCH SIMPLE
 		ON UPDATE NO ACTION ON DELETE NO ACTION
--- system_user/system_user_database/install_system_user_schema_and_objects.bat
+++ system_user/system_user_database/install_system_user_schema_and_objects.bat
@@ -1,48 +1,45 @@
 REM this script installs the application's database and then the system_user_schema and its objects
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres  -p 5432 -f create_report_manager_database.sql 1> C:\Reporting_application\database\install_system_user_schema_and_objects_1.log 2>&1 
-
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f install_pgcrypto.sql  1>> install_system_user_schema_and_objects_1.log 2>>&1 
 
 REM install the schema
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\create_system_user_schema.sql  1> install_system_user_schema_and_objects.log 2>&1 
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\create_system_user_schema.sql  1> install_system_user_schema_and_objects.log 2>&1 
 
 REM install the objects
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\table_defintions\system_users.sql  1>> install_system_user_schema_and_objects.log 2>>&1 
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\table_definitions\system_users.sql  1>> install_system_user_schema_and_objects.log 2>>&1 
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\table_defintions\system_users_history.sql  1>> install_system_user_schema_and_objects.log 2>>&1 
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\table_definitions\system_users_history.sql  1>> install_system_user_schema_and_objects.log 2>>&1 
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\table_defintions\system_user_state.sql 1>> install_system_user_schema_and_objects.log 2>>&1 
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\table_definitions\system_user_state.sql 1>> install_system_user_schema_and_objects.log 2>>&1 
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\table_defintions\system_user_state_history.sql 1>> install_system_user_schema_and_objects.log 2>>&1 
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\table_definitions\system_user_state_history.sql 1>> install_system_user_schema_and_objects.log 2>>&1 
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\table_defintions\system_actions.sql 1>> install_system_user_schema_and_objects.log 2>>&1 
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\table_definitions\system_actions.sql 1>> install_system_user_schema_and_objects.log 2>>&1 
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\table_defintions\system_user_allowed_actions.sql 1>> install_system_user_schema_and_objects.log 2>>&1 
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\table_definitions\system_user_allowed_actions.sql 1>> install_system_user_schema_and_objects.log 2>>&1 
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\table_defintions\system_user_allowed_actions_history.sql 1>> install_system_user_schema_and_objects.log 2>>&1 
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\table_definitions\system_user_allowed_actions_history.sql 1>> install_system_user_schema_and_objects.log 2>>&1 
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\action_functions\action_add_new_user.sql 1>> install_system_user_schema_and_objects.log 2>>&1
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\action_functions\action_add_new_user.sql 1>> install_system_user_schema_and_objects.log 2>>&1
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\action_functions\action_change_password.sql 1>> install_system_user_schema_and_objects.log 2>>&1
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\action_functions\action_change_password.sql 1>> install_system_user_schema_and_objects.log 2>>&1
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\action_functions\action_change_user_allowed_actions.sql 1>> install_system_user_schema_and_objects.log 2>>&1
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\action_functions\action_change_user_allowed_actions.sql 1>> install_system_user_schema_and_objects.log 2>>&1
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\action_functions\action_user_login.sql 1>> install_system_user_schema_and_objects.log 2>>&1
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\action_functions\action_user_login.sql 1>> install_system_user_schema_and_objects.log 2>>&1
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\action_functions\action_user_logout.sql 1>> install_system_user_schema_and_objects.log 2>>&1
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\action_functions\action_user_logout.sql 1>> install_system_user_schema_and_objects.log 2>>&1
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\utility_functions\util_get_user_state.sql 1>> install_system_user_schema_and_objects.log 2>>&1
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\utility_functions\util_get_user_state.sql 1>> install_system_user_schema_and_objects.log 2>>&1
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\utility_functions\util_is_user_authorized.sql 1>> install_system_user_schema_and_objects.log 2>>&1 
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\utility_functions\util_is_user_authorized.sql 1>> install_system_user_schema_and_objects.log 2>>&1 
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\utility_functions\trig_log_state_history.sql 1>> install_system_user_schema_and_objects.log 2>>&1
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\utility_functions\trig_log_state_history.sql 1>> install_system_user_schema_and_objects.log 2>>&1
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\utility_functions\trig_log_allowed_actions_history.sql 1>> install_system_user_schema_and_objects.log 2>>&1
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\utility_functions\trig_log_allowed_actions_history.sql 1>> install_system_user_schema_and_objects.log 2>>&1
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\utility_functions\trig_log_user_history.sql 1>> install_system_user_schema_and_objects.log 2>>&1
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\utility_functions\trig_log_user_history.sql 1>> install_system_user_schema_and_objects.log 2>>&1
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\utility_functions\install_master_user.sql 1> install_master_user.log 2>&1
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\utility_functions\install_master_user.sql 1> install_master_user.log 2>&1
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\Reporting_application\system_user\system_user_database\utility_functions\run_install_master_user.sql 1>> install_master_user.log 2>>&1
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\system_user\system_user_database\utility_functions\run_install_master_user.sql 1>> install_master_user.log 2>>&1
 
