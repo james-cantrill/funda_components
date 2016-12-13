ab767c4510c4ddf50abaa0d1f07550c3ad3687de
Jim Cantrill <CantriJ@DB0NYY1.ccst.ccdor.local>
2016-12-13 15:53:45 -0500

    add programs_manager actions

--- programs_manager/programs_manager_database/table_definitions/insert_system_actions_for_prgrm_mngr.sql
+++ programs_manager/programs_manager_database/table_definitions/insert_system_actions_for_prgrm_mngr.sql
@@ -4,4 +4,8 @@
 INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('programs_manager', 'change_continuum_user_visibility', 'Continuum Visibility');
 INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('programs_manager', 'change_organization_user_visibility', 'Organization Visibility');
 INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('programs_manager', 'change_program_user_visibility', 'Program Visibility');
+INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('programs_manager', 'list_user_visible_programs', 'Program Visibility');
+INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('programs_manager', 'list_user_visible_organizations', 'Program Visibility');
+INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('programs_manager', 'enter_edit_program_folders', 'Program Visibility');
+INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('programs_manager', 'enter_edit_programs', 'Program Visibility');
 
