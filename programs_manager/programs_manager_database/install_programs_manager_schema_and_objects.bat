627e6d19e8ab6eaea0dd09b92d96937301f5712a
Jim Cantrill <CantriJ@DB0NYY1.ccst.ccdor.local>
2016-12-16 14:04:55 -0500

    insert STEPS organizations and programs

--- programs_manager/programs_manager_database/install_programs_manager_schema_and_objects.bat
+++ programs_manager/programs_manager_database/install_programs_manager_schema_and_objects.bat
@@ -37,6 +37,11 @@ REM install the objects
 
 "C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\programs_manager\programs_manager_database\utility_functions\trig_log_user_allowed_programs_history.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
 
+REM add master user
 "C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\programs_manager\programs_manager_database\utility_functions\insert_master_user_report_manager_actions.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
 
 "C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\programs_manager\programs_manager_database\utility_functions\run_insert_master_user_prgrm_mngr_actions.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
+REM add organizations, folders,and programs
+"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f C:\funda_components\programs_manager\programs_manager_database\table_definitions\insert_organizations_program_folders_and_programs.sql  1>> install_programs_manager_schema_and_objects.log 2>>&1 
+
