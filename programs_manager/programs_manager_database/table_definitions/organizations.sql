b101e897f847373074681b218af80fd0d4fa34b1
Jim Cantrill <CantriJ@DB0NYY1.ccst.ccdor.local>
2016-12-16 14:05:27 -0500

    correct error in field name

--- programs_manager/programs_manager_database/table_definitions/organizations.sql
+++ programs_manager/programs_manager_database/table_definitions/organizations.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS programs_manager_schema.organizations;
 CREATE TABLE programs_manager_schema.organizations (
 	organization_id	text  NOT NULL PRIMARY KEY,
 	organization_name	text,
-	datetime_program_changed	timestamp without time zone NOT NULL,
+	datetime_organization_changed	timestamp without time zone NOT NULL,
 	changed_by_user_login	text NOT NULL,
     CONSTRAINT organization_name_duplicate UNIQUE(organization_name),
 	CONSTRAINT organizations_changed_by_user_login_fkey FOREIGN KEY (changed_by_user_login)
