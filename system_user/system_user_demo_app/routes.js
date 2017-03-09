f6b2a6df467d6f02fe9ee4a253389e7bf2f9ff2f
Jim Cantrill <jim.cantrill@dor.org>
2017-03-09 15:12:20 -0500

    Add routing for enter_user_data, add_one_user,and logout
    cleanup debugging comments

--- system_user/system_user_demo_app/routes.js
+++ system_user/system_user_demo_app/routes.js
@@ -1,6 +1,8 @@
 
 var express = require('express');
 var router = express.Router();
+var user_name;
+var user_login;
 
 var seneca = require('seneca')();
 var web = require('seneca-web');
@@ -16,7 +18,7 @@ router.get('/', function(req, res){
 });
 
 router.get('/system_user_manager/user_login', function (req, res) {
-	console.log(req.query);
+	user_login = req.query.login;
 	act ({  role:'system_user_manager', 
 	        cmd:'user_login', 
 			_in_data: {
@@ -25,14 +27,103 @@ router.get('/system_user_manager/user_login', function (req, res) {
 			}
 		})
 	  .then (function (result) {
-		console.log (JSON.stringify (result));
-		console.log ('result = ' + result.action_user_login);
-		res.send (JSON.stringify (result));
+		var returned_json = JSON.parse (JSON.stringify (result));
+		var out_data = returned_json[0].action_user_login;
+		user_name = out_data.firstname + ' ' + out_data.lastname;
+		
+		if (out_data.result_indicator == 'Failure') {
+			res.render ('index', {
+				title: out_data.message
+			});
+		} else {
+			res.render ('home', {
+						name: user_name,
+						title: 'Welcome'
+					});
+		};
+		
 	  })
 	  .catch (function (err) {
 		console.log("ERROR:", err); 
 		res.send ("ERROR:", err);
 	  });	  
-});
+});	//END router.get('/system_user_manager/user_login',
+
+router.get('/enter_user_data', function(req, res){
+  res.render('user_data', {
+        title: "Enter User Data",
+		firstname:	"",
+		lastname:	"",
+		login:	"",
+		Password:	""
+  });
+});  // end of /enter_unsheltered
+
+router.get('/system_user_manager/add_one_user', function (req, res) {
+	
+	act ({  role:'system_user_manager', 
+	        cmd:'add_one_user', 
+			_in_data: {
+					firstname: req.query.firstname,
+					lastname: req.query.lastname,
+					login: req.query.login,
+					password:  req.query.password,
+					changing_user_login: user_login
+			}
+		})
+	  .then(function (result) {
+		var returned_json = JSON.parse (JSON.stringify (result));
+		var out_data = returned_json[0].action_add_new_user;
+		
+		if (out_data.result_indicator == 'Failure') {
+			res.render ('user_data', {
+				title: out_data.message,
+				firstname:	req.query.firstname,
+				lastname:	req.query.lastname,
+				login:	req.query.login,
+				Password:	req.query.password
+			});
+		} else {
+			res.render ('home', {
+						title: 'User ' + req.query.firstname + ' ' + req.query.lastname + ' is added.'
+					});
+		};
+
+	  })
+	  .catch(function (err) {
+		console.log("ERROR:", err); 
+		res.send ("ERROR:", err);
+	  });
+});	//END router.get('/system_user_manager/add_one_user',
+
+router.get('/system_user_manager/user_logout', function (req, res) {
+	act ({  role:'system_user_manager', 
+	        cmd:'user_logout', 
+			_in_data: {
+					login: user_login
+			}
+		})
+	  .then (function (result) {
+		var returned_json = JSON.parse (JSON.stringify (result));
+		var out_data = returned_json[0].action_user_logout;
+		
+		if (out_data.result_indicator == 'Failure') {
+			res.render ('index', {
+						name: user_name,
+						title: out_data.message
+			});
+		} else {
+			res.render ('index', {
+						name: user_name,
+						title: 'You have successfully Logged Out!'
+					});
+		};
+		
+	  })
+	  .catch (function (err) {
+		console.log("ERROR:", err); 
+		res.send ("ERROR:", err);
+	  });	  
+}); //END router.get('/system_user_manager/user_logout'
 
  module.exports = router;
