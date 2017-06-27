1683e97f9f36719481a5bd4980113c49f97a76ec
James E. Cantrill <jcantrill33@gmail.com>
2017-06-27 09:48:55 -0400

    Delete clear_all_tables.sql

--- system_user/system_user_manager/test/clear_all_tables.sql
+++
@@ -1,8 +0,0 @@
-
---dummy
--- clear all tables
-DELETE  FROM system_user_schema.system_users;
-DELETE  FROM system_user_schema.system_user_state;
-DELETE  FROM system_user_schema.system_user_state_history;
-DELETE  FROM system_user_schema.system_user_allowed_actions;
-DELETE  FROM system_user_schema.system_user_allowed_actions_history;
