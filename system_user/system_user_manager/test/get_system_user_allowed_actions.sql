bd6b58f5ab7b49820e317eeb552c75df9f34a8f6
James E. Cantrill <jcantrill33@gmail.com>
2017-06-27 09:48:43 -0400

    Delete get_system_user_allowed_actions.sql

--- system_user/system_user_manager/test/get_system_user_allowed_actions.sql
+++
@@ -1,9 +0,0 @@
--- get_system_user_schema.system_user_allowed_actions.sql
-
-SELECT * 
-FROM system_user_schema.system_user_allowed_actions
-ORDER BY 
-	sysuser_id,
-	datetime_action_allowed_changed
-;
-
