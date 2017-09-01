8ff372dc5a3480e0949d1f945cee9a84f46c25d9
Jim Cantrill <jim.cantrill@dor.org>
2017-09-01 16:15:21 -0400

    change action_display_names fortter understanding

--- system_user/system_user_database/table_definitions/system_actions.sql
+++ system_user/system_user_database/table_definitions/system_actions.sql
@@ -10,7 +10,7 @@ CREATE TABLE system_user_schema.system_actions (
 
 -- insert the actions for the system_user service that require authorization (login and logout don't). The other services will insert their actions as part of their installation
 INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('system_user', 'add_new_user', 'Add User');
-INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('system_user', 'enter_edit_allowed_actions', 'Allowed Actions');
+INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('system_user', 'enter_edit_allowed_actions', 'Edit Allowed User Actions');
 INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('system_user', 'change_password', 'Change Password');
 INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('system_user', 'list_user_actions', 'List User Actions');
 INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('system_user', 'user_login', 'User Login');
