d99d8aacb27366cfc8dc24f7053e98ca176f8313
Jim Cantrill <jim.cantrill@dor.org>
2017-09-18 15:36:57 -0400

    Add variable for password
    Replace hardcoded password

--- system_user/system_user_database/utility_functions/install_master_user.sql
+++ system_user/system_user_database/utility_functions/install_master_user.sql
@@ -1,14 +1,14 @@
 -- The function system_user_schema.install_master_user adds a master user to
 -- the application during database installation
 
-CREATE OR REPLACE FUNCTION system_user_schema.install_master_user () RETURNS integer 
+CREATE OR REPLACE FUNCTION system_user_schema.install_master_user (_master text) RETURNS integer
 AS $$
 
 DECLARE
-		
+
 	_integer_var	integer;
 	_sysuser_id	uuid;
-	
+
 BEGIN
 
 	INSERT INTO system_user_schema.system_users (
@@ -21,21 +21,21 @@ BEGIN
 		)
 	SELECT
 		'muser',
-		(SELECT crypt('ha_melech!16', gen_salt('bf', 8))),
+		(SELECT crypt(_master, gen_salt('bf', 8))),
 		'Master',
 		'User',
 		LOCALTIMESTAMP (0),
 		'Install'
 	;
-	
-	
-	_sysuser_id := (	SELECT 
-							sysuser_id 
-						FROM system_user_schema.system_users 
+
+
+	_sysuser_id := (	SELECT
+							sysuser_id
+						FROM system_user_schema.system_users
 						WHERE login = 'muser')
 						;
-		
-			
+
+
 	INSERT INTO system_user_schema.system_user_state (
 		sysuser_id,
 		user_state,
@@ -46,7 +46,7 @@ BEGIN
 		'Logged Out',
 		LOCALTIMESTAMP (0)
 	;
-	
+
 	--set up all system acttions as allowed for this user
 	INSERT INTO system_user_schema.system_user_allowed_actions (
 		sysuser_id,
@@ -69,10 +69,10 @@ BEGIN
 		LOCALTIMESTAMP (0)
 	FROM	system_user_schema.system_actions
 	;
-		
+
 	_integer_var := 1;
 	RETURN _integer_var;
-	
-	
+
+
 END;
-$$ LANGUAGE plpgsql;		
+$$ LANGUAGE plpgsql;
