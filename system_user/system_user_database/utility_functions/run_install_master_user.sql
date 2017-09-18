067a74ae8209c562a9c3d1d3a5ab29e92941fa6f
Jim Cantrill <jim.cantrill@dor.org>
2017-09-18 15:38:47 -0400

    add v ariable flor password

--- system_user/system_user_database/utility_functions/run_install_master_user.sql
+++ system_user/system_user_database/utility_functions/run_install_master_user.sql
@@ -1,10 +1,9 @@
 -- The function system_user_schema.install_master_user adds a master user to
 -- the application during database installation
 
-SELECT * FROM system_user_schema.install_master_user ();
+SELECT * FROM system_user_schema.install_master_user ('master');
 
 
 -- SELECT * FROM system_user_schema.util_is_user_authorized ('{"login": "muser", "service":"system_user", "action": "add_new_user"}');
 
 -- SELECT * FROM system_user_schema.action_add_new_user ('{"firstname": "Roger", "lastname": "Fitzroger", "login": "opal", "password":"spade", "changing_user_login": "muser"}');
-
