cd1c7cb90b7c810eb98866e353a6c91795c6223c
Jim Cantrill <jim.cantrill@dor.org>
2018-02-19 13:28:43 -0500

    Deleted

--- programs_manager/programs_manager_database/action_functions/test/test_action_enter_edit_program_folders.bat
+++
@@ -1,6 +0,0 @@
-
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f ..\action_enter_edit_program_folders.sql 1> test_action_enter_edit_program_folders.log 2>&1 
-
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f test_action_enter_edit_program_folders.sql  1>> test_action_enter_edit_program_folders.log 2>>&1 
-
- 
