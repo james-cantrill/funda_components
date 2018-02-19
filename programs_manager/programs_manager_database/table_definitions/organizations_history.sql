2928df234f6a77b3c81049eff318e2aa15397b87
Jim Cantrill <jim.cantrill@dor.org>
2018-02-19 14:39:10 -0500

    Initial commit

---
+++ programs_manager/programs_manager_database/table_definitions/organizations_history.sql
@@ -0,0 +1,17 @@
+
+ -- organizations_history.sql
+DROP TABLE IF EXISTS programs_manager_schema.organizations_history;
+
+CREATE TABLE programs_manager_schema.organizations_history (
+	organization_id	uuid NOT NULL,
+	organization_name	text NOT NULL,
+	organization_description	text NOT NULL,
+	datetime_organization_changed	timestamp without time zone NOT NULL,
+	changing_user_login	text NOT NULL,
+	datetime_organization_change_started	timestamp without time zone NOT NULL,
+	datetime_organization_change_ended	timestamp without time zone NOT NULL,   
+	change_type	text NOT NULL, -- the change_type will be either update or delete
+    UNIQUE (organization_id, datetime_organization_changed, changing_user_login, change_type)
+);
+
+
---
+++ programs_manager/programs_manager_database/table_definitions/programs_history.sql
@@ -0,0 +1,21 @@
+
+DROP TABLE  IF EXISTS programs_manager_schema.programs_history;
+
+CREATE TABLE programs_manager_schema.programs_history (
+	program_id	uuid NOT NULL,
+	program_name	text NOT NULL,
+	program_description 	text,
+	organization_id	uuid NOT NULL,
+	containing_organization_level_id	uuid NOT NULL,
+	datetime_program_changed	timestamp without time zone NOT NULL,
+	changing_user_login	text NOT NULL,
+	datetime_user_change_started	timestamp without time zone NOT NULL,
+	datetime_user_change_ended	timestamp without time zone NOT NULL, 
+	change_type	text NOT NULL, -- the change_type will be either update or delete
+    UNIQUE (program_id, datetime_program_changed, changing_user_login )
+);
+
+
+
+
+
