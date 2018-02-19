cbcd249040510a90bd4648152170f9670003794a
Jim Cantrill <jim.cantrill@dor.org>
2018-02-19 16:19:58 -0500

    add action_enter_edit_organizations

--- programs_manager/programs_manager_database/table_definitions/insert_system_actions_for_prgrm_mngr.sql
+++ programs_manager/programs_manager_database/table_definitions/insert_system_actions_for_prgrm_mngr.sql
@@ -2,6 +2,7 @@
 
 -- insert the actions for the programs_manager service that require authorization. The other services will insert their actions as part of their installation
 
+INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('programs_manager', 'action_enter_edit_organizations', 'Enter or Edit the Organizations');
 INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('programs_manager', 'enter_edit_organization_levels', 'Enter or Edit the Organization Levels');
 INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('programs_manager', 'enter_edit_programs', 'Enter or Edit the Programs');
 INSERT INTO system_user_schema.system_actions (service, action, action_display_name) VALUES ('programs_manager', 'change_program_user_visibility', 'Program Visibility');
