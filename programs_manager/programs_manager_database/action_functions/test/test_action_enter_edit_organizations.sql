81356b6865eaa17c2ff32ca636f775ef963eb8b1
Jim Cantrill <jim.cantrill@dor.org>
2018-02-21 13:58:39 -0500

    added tests for using organization_id in updates

--- programs_manager/programs_manager_database/action_functions/test/test_action_enter_edit_organizations.sql
+++ programs_manager/programs_manager_database/action_functions/test/test_action_enter_edit_organizations.sql
@@ -24,13 +24,14 @@
 SELECT * FROM system_user_schema.action_user_login ('{"login": "muser", "password":"master"}');
 
 -- Test 1 incomplete data organization_name
+
 DO $$
 DECLARE  _output_json	json;
 BEGIN	
 	RAISE NOTICE 'TEST 1 incomplete data, organization_name, expected result: Failure';
 	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations ('{ "organization_description": "This is Test Organization 1",  "changing_user_login": "muser", "enter_or_update": "Enter"}'));
 	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
-	RAISE NOTICE '_output_json = %', _output_json;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
 	RAISE NOTICE '';
 END$$;
 
@@ -41,7 +42,7 @@ BEGIN
 	RAISE NOTICE 'TEST 2 incomplete data, organization_description, expected result: Failure';
 	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations ('{ "organization_name": "Test Organization  2",   "changing_user_login": "muser", "enter_or_update": "Enter"}'));
 	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
-	RAISE NOTICE '_output_json = %', _output_json;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
 	RAISE NOTICE '';
 END$$;
 
@@ -52,7 +53,7 @@ BEGIN
 	RAISE NOTICE 'TEST incomplete data, changing_user_login, expected result: Failure';
 	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations ('{"organization_name": "Test Organization  1", "organization_description": "This is Test Organization 1", "enter_or_update": "Enter" }'));
 	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
-	RAISE NOTICE '_output_json = %', _output_json;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
 	RAISE NOTICE '';
 END$$;
 
@@ -63,7 +64,7 @@ BEGIN
 	RAISE NOTICE 'TEST 4 incomplete data, enter_or_update, expected result: Failure';
 	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations ('{ "organization_name": "Test Organization  2", "organization_description": "This is Test Organization 1",  "changing_user_login": "muser"}'));
 	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
-	RAISE NOTICE '_output_json = %', _output_json;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
 	RAISE NOTICE '';
 END$$;
 
@@ -75,41 +76,62 @@ BEGIN
 	RAISE NOTICE 'TEST 2 user isn''t authorized expected result: Failure';
 	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations ('{"organization_name": "Test Organization  1", "organization_description": "Tnis is Test Organization 1",  "changing_user_login": "other",  "enter_or_update": "Enter"}'));
 	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
-	RAISE NOTICE '_output_json = %', _output_json;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
 	RAISE NOTICE '';
 END$$;
 
--- Test 6 successful insert
 DO $$
 DECLARE  _output_json	json;
+	_inserted_org_id	uuid;
+	_in_json	json;
+BEGIN	
+	RAISE NOTICE 'PRE-TEST 6 insert fikrstganization expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations ('{"organization_name": "Orig Test Organization  1", "organization_description": "This is Test Organization 1",  "changing_user_login": "muser", "enter_or_update": "Enter"}'));
+		PERFORM  pg_sleep(5);
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;
+
+
+-- Test 6 successful insert and Test 7 successful update
+DO $$
+DECLARE  _output_json	json;
+	_inserted_org_id	uuid;
+	_in_json	json;
 BEGIN	
 	RAISE NOTICE 'TEST 6 insert expected result: Success';
 	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations ('{"organization_name": "Test Organization  1", "organization_description": "This is Test Organization 1",  "changing_user_login": "muser", "enter_or_update": "Enter"}'));
 	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
-	RAISE NOTICE '_output_json = %', _output_json;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
 	RAISE NOTICE '';
-END$$;
-
-
--- Test 7 successful update
-DO $$
-DECLARE  _output_json	json;
-BEGIN	
-	RAISE NOTICE 'TEST 7 successful update expected result: Success';
-	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations ('{"organization_name": "Test Organization  1", "organization_description": "This is a new Test Organization 1",  "changing_user_login": "muser", "enter_or_update": "Update"}'));
+	
+	_inserted_org_id := (SELECT _output_json ->> 'organization_id')::uuid;
+	RAISE NOTICE '_inserted_org_id = %', _inserted_org_id;
+	_in_json := '{"organization_id": "' || _inserted_org_id::text || '", "organization_name": "Test Organization  1", "organization_description": "This is Test Organization 1",  "changing_user_login": "muser", "enter_or_update": "Update"}';
+	--RAISE NOTICE '_in_json = %', _in_json;
+	
+	PERFORM  pg_sleep(5);
+	RAISE NOTICE 'Sleep over.';
+	
+	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations ('{"organization_name": "Test Organization  2", "organization_description": "This is Test Organization 2",  "changing_user_login": "muser", "enter_or_update": "Enter"}'));
+	
+	RAISE NOTICE 'Test 7 successful update expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations (_in_json));
 	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
-	RAISE NOTICE '_output_json = %', _output_json;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
 	RAISE NOTICE '';
 END$$;
+
 
 -- Test 8 insert of already existing organization
 DO $$
 DECLARE  _output_json	json;
 BEGIN	
-	RAISE NOTICE 'TEST8 insert of already existing organization expected result: Failure';
-	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations ('{"organization_name": "Test Organization  1", "organization_description": "This is a ne organization, Test Organization 1",  "changing_user_login": "muser", "enter_or_update": "Enter"}'));
+	RAISE NOTICE 'TEST 8 insert of already existing organization expected result: Failure';
+	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations ('{"organization_name": "Test Organization  1", "organization_description": "This is a new organization, Test Organization 1",  "changing_user_login": "muser", "enter_or_update": "Enter"}'));
 	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
-	RAISE NOTICE '_output_json = %', _output_json;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
 	RAISE NOTICE '';
 END$$;
 
@@ -120,6 +142,46 @@ BEGIN
 	RAISE NOTICE 'TEST 9 invalid value of enter_or_update expected result: Failure';
 	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations ('{"organization_name": "Test Organization  3", "organization_description": "This is a new organization, Test Organization 3",  "changing_user_login": "muser", "enter_or_update": "Other"}'));
 	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
-	RAISE NOTICE '_output_json = %', _output_json;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
 	RAISE NOTICE '';
 END$$;
+
+-- Test 10 missing organization_id for update
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'TEST 10 update with a missing organization_id expected result: Failure';
+	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations ('{"organization_name": "Test Organization  1", "organization_description": " Test Organization 1 is nogt a new organization",  "changing_user_login": "muser", "enter_or_update": "Update"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;
+
+-- Test 12 extension of Test 6 and 7 successful update
+DO $$
+DECLARE  _output_json	json;
+		_org_id	uuid;
+		_in_json	json;
+		
+BEGIN	
+
+	_org_id := (SELECT 
+					organization_id
+				FROM	programs_manager_schema.organizations
+				WHERE	organization_name = 'Test Organization  1' );
+	RAISE NOTICE '_org_id = %', _org_id;
+	
+	_in_json := '{"organization_id": "' || _org_id::text || '", "organization_name": "Test Organization  1", "organization_description": "This is UPDATED Test Organization 1",  "changing_user_login": "muser", "enter_or_update": "Update"}';
+	RAISE NOTICE 'Test 12 extension of Test 6 and 7 successful update expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations (_in_json));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;
+
+
+SELECT '';
+SELECT * FROM programs_manager_schema.organizations;
+
+SELECT '';
+SELECT * FROM programs_manager_schema.organizations_history;
