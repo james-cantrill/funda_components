6d2cf9a2b1781ee755da9dcc5217a229b635b39a
Jim Cantrill <jim.cantrill@dor.org>
2018-02-21 13:46:59 -0500

    start tests by installing DB fresh

--- programs_manager/programs_manager_database/action_functions/test/test_action_enter_edit_organizations.bat
+++ programs_manager/programs_manager_database/action_functions/test/test_action_enter_edit_organizations.bat
@@ -1,3 +1,6 @@
+REM install db
+call C:\funda_components\programs_manager\programs_manager_database\program_manager_test_database\install_program_manager_test_db.bat
+
 REM test_action_enter_edit_organizations
 
 "C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d program_manager_test_db  -p 5432 -f ..\action_enter_edit_organizations.sql 1> test_action_enter_edit_organizations.log 2>&1 
