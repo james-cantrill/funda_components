a2e1e0fa5f47882e6af5539e484269966114e4f3
Jim Cantrill <jim.cantrill@dor.org>
2018-02-22 16:22:26 -0500

    update

--- programs_manager/programs_manager_database/action_functions/test/test_action_enter_edit_programs.bat
+++ programs_manager/programs_manager_database/action_functions/test/test_action_enter_edit_programs.bat
@@ -1,7 +1,11 @@
+REM install db
+call C:\funda_components\programs_manager\programs_manager_database\program_manager_test_database\install_program_manager_test_db.bat
+
 REM test_action_enter_edit_programs
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f ..\action_enter_edit_programs.sql 1> test_action_enter_edit_programs.log 2>&1 
-REM "C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f test_action_enter_edit_program_folders.sql  1>> test_action_enter_edit_programs.log
+"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d program_manager_test_db  -p 5432 -f ..\action_enter_edit_programs.sql 1> test_action_enter_edit_programs.log 2>&1 
+
+
  
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f test_action_enter_edit_programs.sql  1>> test_action_enter_edit_programs.log 2>>&1 
+"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d program_manager_test_db  -p 5432 -f test_action_enter_edit_programs.sql  1>> test_action_enter_edit_programs.log 2>>&1 
 
  
