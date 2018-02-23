9643f240e93dcee1693826d13e279b273baf9922
Jim Cantrill <jim.cantrill@dor.org>
2018-02-23 10:27:38 -0500

    Add history trigger installation

--- programs_manager/programs_manager_database/action_functions/test/test_action_enter_edit_programs.bat
+++ programs_manager/programs_manager_database/action_functions/test/test_action_enter_edit_programs.bat
@@ -4,7 +4,7 @@ call C:\funda_components\programs_manager\programs_manager_database\program_mana
 REM test_action_enter_edit_programs
 "C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d program_manager_test_db  -p 5432 -f ..\action_enter_edit_programs.sql 1> test_action_enter_edit_programs.log 2>&1 
 
-
+"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d program_manager_test_db  -p 5432 -f C:\funda_components\programs_manager\programs_manager_database\utility_functions\trig_log_programs_history.sql 1>> test_action_enter_edit_programs.log 2>>&1 
  
 "C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d program_manager_test_db  -p 5432 -f test_action_enter_edit_programs.sql  1>> test_action_enter_edit_programs.log 2>>&1 
 
