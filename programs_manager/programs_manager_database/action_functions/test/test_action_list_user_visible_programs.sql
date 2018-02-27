4058533f5c1e0c2b8220c69908b5f520da221999
Jim Cantrill <jim.cantrill@dor.org>
2018-02-27 11:37:38 -0500

    Update to include refactoring changes to the action_list_user_visible function

--- programs_manager/programs_manager_database/action_functions/test/test_action_list_user_visible_programs.sql
+++ programs_manager/programs_manager_database/action_functions/test/test_action_list_user_visible_programs.sql
@@ -1,51 +1,212 @@
 -- test_action_list_user_visible_programs.sql
 
-		
--- SET UP the test users
-	SELECT * FROM system_user_schema.action_user_login ('{"login": "muser", "password":"ha_melech!16"}');
-	SELECT * FROM system_user_schema.action_add_new_user ('{"firstname": "Roger", "lastname": "Fitzroger", "login": "opal", "password":"spade", "changing_user_login": "muser"}');
-	SELECT * FROM system_user_schema.action_add_new_user ('{"firstname": "John", "lastname": "Appleton", "login": "jappl", "password":"river", "changing_user_login": "muser"}');
-	
--- SET UP the reports allowed
-	SELECT * FROM programs_manager_schema.action_change_program_user_visibility ('{"login": "jappl", "service":"programs_manager", "program":"CCST Emergency Services Food/Clothing", "task":"visible", "changing_user_login": "muser"}');
-	SELECT * FROM programs_manager_schema.action_change_program_user_visibility ('{"login": "jappl", "service":"programs_manager", "program":"ACCORD SHP Transitional Housing", "task":"visible", "changing_user_login": "muser"}');
-	SELECT * FROM programs_manager_schema.action_change_program_user_visibility ('{"login": "jappl", "service":"programs_manager", "program":"Arbor Hospital Diversion 212", "task":"visible", "changing_user_login": "muser"}');
-	SELECT * FROM programs_manager_schema.action_change_program_user_visibility ('{"login": "jappl", "service":"programs_manager", "program":"CCST Supported Housing LTS", "task":"visible", "changing_user_login": "muser"}');
-	SELECT * FROM programs_manager_schema.action_change_program_user_visibility ('{"login": "jappl", "service":"programs_manager", "program":"CCST Shelter Plus Care - HUD Permanent", "task":"visible", "changing_user_login": "muser"}');
-	SELECT * FROM programs_manager_schema.action_change_program_user_visibility ('{"login": "jappl", "service":"programs_manager", "program":"CCST Supported Housing MICA", "task":"visible", "changing_user_login": "muser"}');
+/*	_in_data:	{
+					login:
+				}
+			
+* The json object returned by the function, _out_json, is defined  below.
 
--- TEST the function
-	SELECT * FROM programs_manager_schema.action_list_user_visible_programs ('{"login": "jappl"}');
-	
-	SELECT * FROM programs_manager_schema.action_list_user_visible_programs ('{"login": "opal"}');
-------------------------------------------------------------------------------------------------------
-	--	CLEAN UP
-		DELETE  FROM programs_manager_schema.system_user_allowed_programs  WHERE login  IN ('opal', 'jappl');
-		DELETE  FROM system_user_schema.system_user_allowed_actions_history WHERE login  IN ('opal', 'jappl');		
-		DELETE  FROM system_user_schema.system_user_allowed_actions WHERE login  IN ('opal', 'jappl');
-		DELETE FROM system_user_schema.system_users WHERE login IN ('opal', 'jappl');
-
-/*
-"ACCORD SHP Transitional Housing"
-"ACCORD Kalthoff House"
-"Arbor Transitional Housing 339"
-"Arbor Hospital Diversion 212"
-"Arbor Supported Housing - OMH Permanent"
-"Arbor Shelter Plus Care HUD Permanent"
-"ACCORD Homeless Housing Program"
-"CCST Homeless Supportive Housing - HUD Permanent"
-"CCST Shelter Plus Care - HUD Permanent"
-"CCST Supported Housing Chemung - OMH Permanent"
-"CCST Supported Housing Schuyler - OMH Permanent"
-"CCST Supported Housing HUD 811 Miller Manor"
-"CCST Supported Housing LTS"
-"CCST Supported Housing MICA"
-"CCST Supported Housing RIV I"
-"CCST Supported Housing RIV II"
-"CCST Supported Housing Schuyler County II"
-"Arbor Allegany Supported Housing - 227"
-"Arbor Allegany OMH Supported Housing - 221"
-"Arbor Shelter Plus Care - Allegany 224/254"
-"Arbor Allegany Special Supported Housing - 251"
-"Arbor PC Long term Supported Housing - 231"
+	_out_json:	{
+				result_indicator:
+				message:
+				login:
+				programs: [
+						{
+							program_id:
+							other_program_id:    -- unique program identifier from outside this system.
+							program_name:
+						}
 */
+		
+--	log in the master user
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'Log in the master userexpected result: Success';
+	_output_json := (SELECT * FROM system_user_schema.action_user_login ('{"login": "muser", "password":"master"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;
+	
+-- SET UP the test users
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'SET UP the test user opal expected result: Success';
+	_output_json := (SELECT * FROM system_user_schema.action_add_new_user ('{"firstname": "Roger", "lastname": "Fitzroger", "login": "opal", "password":"spade", "changing_user_login": "muser"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;
+
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'SET UP the test user jappl expected result: Success';
+	_output_json := (SELECT * FROM system_user_schema.action_add_new_user ('{"firstname": "John", "lastname": "Appleton", "login": "jappl", "password":"river", "changing_user_login": "muser"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;
+
+--	insert an organization, an organization level and a program
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'insert an organization expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organizations ('{"organization_name": "Test Organization 1", "organization_description": "This is Test Organization 1",  "changing_user_login": "muser", "enter_or_update": "Enter"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;
+
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'Insert an organizatipn level expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_organization_level ('{"organization_level_name": "program_root", "organization_level_display_name":"All Programs", "organization_level_description":"List of all programs", "is_root_level": "TRUE", "organization_name": "Test Organization 1", "changing_user_login": "muser", "enter_or_update": "Enter"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;		
+
+-- insert 5 programs		
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'Insert A Good Program expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_programs ('{"program_name": "A Good Program", "program_description": "This is a descriptipon", "other_program_id": "70014021", "organization_name":"Test Organization 1", "containing_level_name": "program_root", "changed_by_user_login": "muser", "enter_or_update": "Enter"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;		
+		
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'Insert A Very Good Program expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_programs ('{"program_name": "A Very Good Program", "program_description": "This is a descriptipon",  "other_program_id": "20009000", "organization_name":"Test Organization 1", "containing_level_name": "program_root", "changed_by_user_login": "muser", "enter_or_update": "Enter"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;		
+		
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'Insert Program 3 expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_programs ('{"program_name": "Program 3", "program_description": "This is a descriptipon",  "other_program_id": "50036000", "organization_name":"Test Organization 1", "containing_level_name": "program_root", "changed_by_user_login": "muser", "enter_or_update": "Enter"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;		
+		
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'Insert Program 4 expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_programs ('{"program_name": "Program 4", "program_description": "This is a descriptipon",  "other_program_id": "50008000", "organization_name":"Test Organization 1", "containing_level_name": "program_root", "changed_by_user_login": "muser", "enter_or_update": "Enter"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;		
+		
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'Insert Program 5 expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_enter_edit_programs ('{"program_name": "Program 5", "program_description": "This is a descriptipon",  "other_program_id": "40008000", "organization_name":"Test Organization 1", "containing_level_name": "program_root", "changed_by_user_login": "muser", "enter_or_update": "Enter"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;		
+		
+
+-- make all five programs visible to user Roger Fitzroger, "login": "opal"
+
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'Make A Good Program visible to opal. expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_change_program_user_visibility ('{"target_login": "opal", "program":"A Good Program", "visible":"TRUE", "changing_user_login": "muser"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;
+
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'Make A Very Good Program visible to opal. expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_change_program_user_visibility ('{"target_login": "opal", "program":"A Very Good Program", "visible":"TRUE", "changing_user_login": "muser"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;
+
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'Make Program 3 visible to opal. expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_change_program_user_visibility ('{"target_login": "opal", "program":"Program 3", "visible":"TRUE", "changing_user_login": "muser"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;
+
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'Make Program 4 visible to opal. expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_change_program_user_visibility ('{"target_login": "opal", "program":"Program 4", "visible":"TRUE", "changing_user_login": "muser"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;
+
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'Make Program 5 visible to opal. expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_change_program_user_visibility ('{"target_login": "opal", "program":"Program 5", "visible":"TRUE", "changing_user_login": "muser"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+END$$;
+
+--------------- Tests of action_list_user_visible_programs -------------------
+
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'Test 1 use is not autnhorized to list visible programs for a user. expected result: Failure';
+	_output_json := (SELECT * FROM programs_manager_schema.action_list_user_visible_programs ('{"login": "jappl", "changing_user_login": "opal"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '_output_json = %', _output_json;
+END$$;
+
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'Test 2 list for user jappl who has no programs visible. expected result: Failure';
+	_output_json := (SELECT * FROM programs_manager_schema.action_list_user_visible_programs ('{"login": "jappl", "changing_user_login":  "muser"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '_output_json = %', _output_json;
+END$$;
+
+DO $$
+DECLARE  _output_json	json;
+BEGIN	
+	RAISE NOTICE 'Test 3 list for user opal who has five programs visible. expected result: Success';
+	_output_json := (SELECT * FROM programs_manager_schema.action_list_user_visible_programs ('{"login": "opal", "changing_user_login":  "muser"}'));
+	RAISE NOTICE 'TEST Result = %', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '_output_json = %', _output_json;
+END$$;
+
+
+
+
