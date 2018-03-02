8dc2469c663ca2d66bc3d1b1d9789d71e737e629
Jim Cantrill <jim.cantrill@dor.org>
2018-03-02 16:23:46 -0500

    Add complete db installation before tests

--- reports_manager/report_manager_database/action_functions/test/test_action_enter_edit_folders.bat
+++ reports_manager/report_manager_database/action_functions/test/test_action_enter_edit_folders.bat
@@ -1,3 +1,5 @@
+REM install db
+call C:\funda_components\reports_manager\report_manager_database\reports_manager_test_db\install_reports_manager_test_db.bat
 
 "C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d reports_manager_test_db  -p 5432 -f ..\action_enter_edit_folders.sql 1> test_action_enter_edit_folders.log 2>&1 
 
