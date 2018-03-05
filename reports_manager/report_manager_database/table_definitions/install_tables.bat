a5d1d937b16e1eb1c20b602866148786b49188dd
Jim Cantrill <jim.cantrill@dor.org>
2018-03-05 15:38:49 -0500

    Delete

--- reports_manager/report_manager_database/table_definitions/install_tables.bat
+++
@@ -1,9 +0,0 @@
-
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f reports.sql 
-
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f parameter_list.sql
-
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f report_parameters.sql
-
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f system_user_allowed_reports.sql
-
