cc662fbd800f6e13eac1b043c2fc00ce397e35c8
Jim Cantrill <jim.cantrill@dor.org>
2018-03-08 16:15:47 -0500

    change target db

--- system_user/system_user_demo_app_vue_js/public/javascript/application_config.js
+++ system_user/system_user_demo_app_vue_js/public/javascript/application_config.js
@@ -1,7 +1,7 @@
 //application_config.js
 
 module.exports = {
-	database_name: 'system_user_demo_db',
+	database_name: 'reports_manager_test_db',
 	db_login: 'postgres',
 	db_password:  'W!dwfame29',
 	db_host: 'localhost',
