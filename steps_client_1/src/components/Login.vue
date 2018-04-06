7b83999d9569f6e554a94fb63813f60f3e4c11d0
Jim Cantrill <jim.cantrill@dor.org>
2018-04-06 16:21:49 -0400

    call system user api login method

--- steps_client_1/src/components/Login.vue
+++ steps_client_1/src/components/Login.vue
@@ -2,23 +2,27 @@
     <div>
         <h1>{{msg}}</h1>
 
-            <form action="/system_user_manager/user_login" method="get" class="form-signin">
+            <form class="form-signin"  @submit.prevent="login">
+
                 <div>
                     <label>Username</label>
                     <br>
-                    <input type="text" name="username" placeholder="" class="input-block-level">
+                    <input type="text" v-model="username" placeholder="" class="input-block-level">
                     <br><br>
                 </div>
+
                 <div>
                     <label>Password</label>
                     <br>
-                    <input type="password" name="password" placeholder="" class="input-block-level">
+                    <input type="password" v-model="password" placeholder="" class="input-block-level">
                     <br><br>
                 </div>
+
                 <div class="row">
                     <button type="submit" class="btn btn-large btn-primary">Sign in </button>
                 </div>
             </form>
+
         </div>
 </template>
 
@@ -28,7 +32,35 @@ export default {
   name: 'Login',
   data () {
     return {
+      username: '',
+      password: '',
+      error: false,
       msg: 'Welcome to STEPS Reports'
+    }
+  },
+
+  methods: {
+    login () {
+      this.axios.get('http://localhost:3000/system_user_manager/user_login?login=' + this.username + '&password=' + this.password)
+        .then(request => this.loginSuccessful(request))
+        .catch(function (error) {
+            console.log('in error')
+			console.log(JSON.stringify(error))
+			//if (error.response) {
+              console.log(error.response.data);
+              console.log(error.response.status);
+              console.log(error.response.headers);
+          //  }
+        })
+    },
+
+    loginSuccessful (req) {
+      if (req.data.result_indicator == 'Failure' ){
+           this.msg = req.data.message;
+          this.$router.replace(this.$route.query.redirect || '/');
+      } else {
+         this.$router.replace(this.$route.query.redirect || '/home')
+      }
     }
   }
 }
