abcbfb989c08eb3ac7f3364dd7a0df207af2af32
Jim Cantrill <jim.cantrill@dor.org>
2018-05-18 16:19:01 -0400

    Defince crude layout and add code to load a selected report

--- steps_client_1/src/components/Home.vue
+++ steps_client_1/src/components/Home.vue
@@ -1,11 +1,27 @@
 <template lang="html">
     <div>
-        <h1>{{hm_msg}}</h1>
-        <v-jstree :data="report_json"  ></v-jstree>
+        <h1>{{wlcm_msg}}</h1>
+
+        <div style="width:900px; margin: 0 auto; ">
+            <div style="width:69%; display:inline-block; vertical-align: top; ">
+                <p style="text-align:left">{{hm_msg}}</p>
+                <v-jstree :data="report_json" @item-click="itemClick" style="width:620px; height:400px; overflow:auto; overflow-x:auto; overflow-y:auto;" ></v-jstree>
+            </div>
+            <div style="width:30%; display:inline-block;">
+                <textarea v-model="itemDescription" style="height:300px; width:100%;">
+                </textarea>
+
+                  <form  class="loadReport" @submit.prevent="loadReport">
+                    <button v-if="reportSelected" class="btn-medium" type="submit">{{runMsg}}  </button>
+                  </form>
+
+            </div>
+        </div>
+
             <form class="logout" @submit.prevent="logout">
                 <button class="btn-medium" type="submit">Log Out</button>
             </form>
-        </div>
+    </div>
 
 </template>
 
@@ -20,6 +36,12 @@ export default {
   data () {
     return {
       hm_msg: 'Please Select a Report',
+      wlcm_msg: 'Welcome ' + globalStore.userFullName,
+      itemDescription: 'Description appears here',
+      reportSelected: 'FALSE',
+      reportId: '',
+      reportName: '',
+      runMsg: '',
       report_json: [],
       localvar: globalStore.userLogin
     }
@@ -30,7 +52,8 @@ export default {
     },
 
   beforeCreate() {
-    console.log('global login = ' + globalStore.userLogin)
+    //console.log('global login = ' + globalStore.userLogin)
+    //console.log('global userFullName = ' + globalStore.userFullName)
     this.axios.get('http://localhost:4000/report_manager/load_report_list?login=' + globalStore.userLogin)
       .then(request => this.reportsReturned(request))
           .catch(function (error) {
@@ -46,12 +69,52 @@ export default {
   },
 
   methods: {  
-  reportsReturned (req) {
-       this.hm_msg = req.data.message;
+    reportsReturned (req) {
+      //this.hm_msg = req.data.message;
        this.report_json = JSON.parse(JSON.stringify (req.data.data));
      },
      
-    logout () {      
+    itemClick (node) {
+      console.log('node.model.icon = ' + node.model.icon)
+      this.reportSelected = 'FALSE'
+      this.reportName = ''
+      this.editingNode = node
+      this.editingItem = node.model
+      this.itemDescription = node.model.value
+      this.reportName = node.model.text
+	  this.reportId = node.model.text
+      console.log('report selected = ' + this.reportSelected)
+      console.log('report name = ' + this.reportName)
+      if (node.model.icon == 'folder') {
+        this.reportSelected = 'FALSE'
+        this.runMsg = ''
+      } else {
+        if (node.model.icon == 'report') {
+          this.reportSelected = 'TRUE'
+          this.runMsg = 'Open ' + this.reportName
+        }
+      }
+      console.log('report selected = ' + this.reportSelected)
+      console.log('report name = ' + this.runMsg)
+    },
+
+     loadReport () {      
+      console.log('in loadReport')    
+      this.axios.get('http://localhost::4000/report_manager/load_selected_report?login=' + globalStore.userLogin + 'report_id=' + this.reportId)
+        .then(request => this.reportLoaded(request))
+        .catch(function (error) {
+          console.log('in error')
+          console.log(JSON.stringify(error))
+        })
+    },
+
+     reportLoaded (req) {
+        this.msg = req.data.message;
+        console.log(JSON.stringify (req));
+       // this.$router.replace(this.$route.query.redirect || '/');
+    },
+
+	logout () {      
       this.axios.get('http://localhost:3000/system_user_manager/user_logout?login=' + globalStore.userLogin)
         .then(request => this.logoutSuccessful(request))
         .catch(function (error) {
@@ -62,7 +125,7 @@ export default {
      
     logoutSuccessful (req) {
         this.msg = req.data.message;
-        console.log(JSON.stringify (req));
+        //console.log(JSON.stringify (req));
         this.$router.replace(this.$route.query.redirect || '/');
     } 
   } 
@@ -70,4 +133,5 @@ export default {
 </script>
 
 <style lang="css">
+
 </style>
