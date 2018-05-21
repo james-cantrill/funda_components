340de7fcedb4d23f14ab04205c7f4e7df08aef66
Jim Cantrill <jim.cantrill@dor.org>
2018-05-21 16:16:43 -0400

    add installation of report manager action functions

--- reports_manager/report_manager_database/reports_manager_test_db/install_reports_manager_test_db.bat
+++ reports_manager/report_manager_database/reports_manager_test_db/install_reports_manager_test_db.bat
@@ -139,10 +139,15 @@ REM Install the action functions and triggers
 
 "C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d reports_manager_test_db  -p 5432 -f C:\funda_components\reports_manager\report_manager_database\utility_functions\trig_log_system_user_allowed_reports_history.sql  1>> install_reports_manager_test_db.log 2>>&1 
 
-"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d reports_manager_test_db  -p 5432 -f C:\funda_components\reports_manager\report_manager_database\action_functions\action_load_report_list_jstree.sql 1> install_reports_manager_test_db.log 2>&1 
-
-
 "C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d reports_manager_test_db  -p 5432 -f C:\funda_components\reports_manager\report_manager_database\utility_functions\pop_allowed_reports_out_data.sql  1>> install_reports_manager_test_db.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d reports_manager_test_db  -p 5432 -f C:\funda_components\reports_manager\report_manager_database\action_functions\action_load_report_list_jstree.sql 1>> install_reports_manager_test_db.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d reports_manager_test_db  -p 5432 -f C:\funda_components\reports_manager\report_manager_database\action_functions\action_load_report_list_vue_jstree.sql 1>> install_reports_manager_test_db.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d reports_manager_test_db  -p 5432 -f C:\funda_components\reports_manager\report_manager_database\action_functions\action_load_selected_report.sql 1>> install_reports_manager_test_db.log 2>>&1 
+
+"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d reports_manager_test_db  -p 5432 -f C:\steps_reporting\report_manager\report_manager_database\utility_functions\util_is_report_viewable_to_user.sql 1>> install_reports_manager_test_db.log 2>>&1 
 
 
 REM  *************************************************************
