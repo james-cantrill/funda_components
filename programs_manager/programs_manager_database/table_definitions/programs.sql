e8b8785e1f5aa1a2edcd75f7c6f7054137edf98f
Jim Cantrill <jim.cantrill@dor.org>
2018-05-23 16:01:32 -0400

    remove fk constraint on (organization_level_id)

--- programs_manager/programs_manager_database/table_definitions/programs.sql
+++ programs_manager/programs_manager_database/table_definitions/programs.sql
@@ -7,15 +7,12 @@ CREATE TABLE programs_manager_schema.programs (
 	program_description 	text,
 	other_program_id	text,	-- unique program identifier from outside this system.
 	organization_id	uuid NOT NULL,
-	containing_organization_level_id	uuid NOT NULL,
+	containing_organization_level_id	uuid,
 	datetime_program_changed	timestamp without time zone NOT NULL,
 	changing_user_login	text NOT NULL,
     CONSTRAINT program_name_duplicate UNIQUE(program_name),
 	CONSTRAINT programs_organization_id_fkey FOREIGN KEY (organization_id)
 		REFERENCES programs_manager_schema.organizations (organization_id) MATCH SIMPLE
-		ON UPDATE NO ACTION ON DELETE CASCADE,
-	CONSTRAINT programs_containing_organization_level_id_fkey FOREIGN KEY (containing_organization_level_id)
-		REFERENCES programs_manager_schema.organization_level (organization_level_id) MATCH SIMPLE
 		ON UPDATE NO ACTION ON DELETE CASCADE,
 	CONSTRAINT programs_changing_user_login_fkey FOREIGN KEY (changing_user_login)
 		REFERENCES system_user_schema.system_users (login) MATCH SIMPLE
