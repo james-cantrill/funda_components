5c0c925d0bfe3316beecbbb86f3b5b318a0c6582
Jim Cantrill <jim.cantrill@dor.org>
2018-07-03 14:08:53 -0400

    Add navbar

--- steps_client_1/src/components/changePword.vue
+++ steps_client_1/src/components/changePword.vue
@@ -1,5 +1,35 @@
 <template lang="html">
     <div>
+      <div style="width:900px; height:150px; margin: 0 auto;  horizontal-align: left">
+        <b-navbar toggleable="md" type="light" variant="light">
+
+          <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+          <b-navbar-brand href="#">Navigation</b-navbar-brand>
+
+          <b-collapse is-nav id="nav_collapse">
+
+            <b-navbar-nav>
+              <b-nav-item to="home" variant="primary">Home</b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+
+              <b-nav-item-dropdown right>
+                <!-- Using button-content slot -->
+                <template slot="button-content">
+                  <em>User</em>
+                </template>
+                <b-dropdown-item v-bind:to="'changePword'" href="#" >Change Password</b-dropdown-item>
+                <b-dropdown-item href="#" v-on:click="logout ()">Signout</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+
+          </b-collapse>
+        </b-navbar>
+
+      </div>
         <h1>Change Password for {{localvar}}</h1>
 
         <div v-if="changePwordFailed" class="loadError">
@@ -66,7 +96,11 @@ methods: {
         this.resultMsg = 'Entered Passwords Do Not Match. Please Try Again'
         this.changePwordFailed = true
       } else {
-        this.axios.get('http://localhost:3000/system_user_manager/user_login?login=' + this.username + '&password=' + this.password)
+        this.username = globalStore.userLogin
+        console.log ('username = ' + this.username)
+        var sent_url = 'http://localhost:3000/system_user_manager/change_password?login=' + this.username + '&password=' + this.password + '&changing_user_login=' + this.username 
+        console.log (sent_url)
+        this.axios.get(sent_url)
           .then(request => this.changePwordSuccessful(request))
           .catch(function (error) {
             console.log('in error')
@@ -77,6 +111,7 @@ methods: {
     },
 
     changePwordSuccessful (req) {
+      console.log ('Returned data = ' + req.data)
       if (req.data.result_indicator == 'Failure'){
         this.resultMsg = req.data.message;
         this.changePwordFailed = true;
--- steps_client_1/src/components/runReport.vue
+++ steps_client_1/src/components/runReport.vue
@@ -1,5 +1,37 @@
 <template lang="html">
     <div>
+
+      <div style="width:900px; height:150px; margin: 0 auto;  horizontal-align: left">
+        <b-navbar toggleable="md" type="light" variant="light">
+
+          <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+          <b-navbar-brand href="#">Navigation</b-navbar-brand>
+
+          <b-collapse is-nav id="nav_collapse">
+
+            <b-navbar-nav>
+              <b-nav-item to="home" variant="primary">Home</b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+
+              <b-nav-item-dropdown right>
+                <!-- Using button-content slot -->
+                <template slot="button-content">
+                  <em>User</em>
+                </template>
+                <b-dropdown-item v-bind:to="'changePword'" href="#" >Change Password</b-dropdown-item>
+                <b-dropdown-item href="#" v-on:click="logout ()">Signout</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+
+          </b-collapse>
+        </b-navbar>
+
+      </div>
+
         <h2>{{run_msg}}</h2>
 
             <div v-if="showReportStartDate > 0" style="width:400; display:inline-block; vertical-align: top;">
