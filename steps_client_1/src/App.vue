619e9ee4126baca5727946fb2e4585efcf0f9287
Jim Cantrill <jim.cantrill@dor.org>
2018-07-03 14:08:20 -0400

    Remove navbar to add to other co0mponents

--- steps_client_1/src/App.vue
+++ steps_client_1/src/App.vue
@@ -9,42 +9,6 @@
             <div class="title" style=" height:70 px; width: 600px; ">STEPS Reports</div>
         </div>
 
-        <b-navbar toggleable="md" type="light" variant="light">
-
-          <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-
-          <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-          <b-collapse is-nav id="nav_collapse">
-
-            <b-navbar-nav>
-              <b-nav-item to="home" variant="primary">Home</b-nav-item>
-              <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            </b-navbar-nav>
-
-            <!-- Right aligned nav items -->
-            <b-navbar-nav class="ml-auto">
-
-              <b-nav-item-dropdown text="Lang" right>
-                <b-dropdown-item href="#">EN</b-dropdown-item>
-                <b-dropdown-item href="#">ES</b-dropdown-item>
-                <b-dropdown-item href="#">RU</b-dropdown-item>
-                <b-dropdown-item href="#">FA</b-dropdown-item>
-              </b-nav-item-dropdown>
-
-              <b-nav-item-dropdown right>
-                <!-- Using button-content slot -->
-                <template slot="button-content">
-                  <em>User</em>
-                </template>
-                <b-dropdown-item v-bind:to="'changePword'" href="#" >Change Password</b-dropdown-item>
-                <b-dropdown-item href="#" v-on:click="logout ()">Signout</b-dropdown-item>
-              </b-nav-item-dropdown>
-            </b-navbar-nav>
-
-          </b-collapse>
-        </b-navbar>
-      <p>  </p><br><br>
     </div>
       <router-view/>
 
