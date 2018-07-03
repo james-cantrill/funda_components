ce21f9ca4cacfc0ee9548936311a3469dbd5c3a5
Jim Cantrill <jim.cantrill@dor.org>
2018-07-03 14:11:06 -0400

    Bring testing up to date

--- system_user/system_user_database/action_functions/test/test_action_user_login.bat
+++ system_user/system_user_database/action_functions/test/test_action_user_login.bat
@@ -1,6 +1,9 @@
 
-"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d system_user_demo_db  -p 5432 -f ..\action_user_login.sql 1> test_action_user_login.log 2>&1 
+REM install db
+call C:\funda_components\system_user\system_user_database\system_user_test_db\install_system_user_test_db.bat
 
-"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d system_user_demo_db  -p 5432 -f test_action_user_login_master.sql  1>> test_action_user_login.log 2>>&1 
+"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d system_user_test_db  -p 5432 -f ..\action_user_login.sql 1> test_action_user_login.log 2>&1 
+
+"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d system_user_test_db  -p 5432 -f test_action_user_login.sql  1>> test_action_user_login.log 2>>&1 
 
  
