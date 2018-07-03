91ec2d363b56270cc9017c2ba678fe9f1df97675
Jim Cantrill <jim.cantrill@dor.org>
2018-07-03 14:11:47 -0400

    Remove extraneous code

--- system_user/system_user_database/utility_functions/run_install_master_user.sql
+++ system_user/system_user_database/utility_functions/run_install_master_user.sql
@@ -3,7 +3,3 @@
 
 SELECT * FROM system_user_schema.install_master_user ('master');
 
-
--- SELECT * FROM system_user_schema.util_is_user_authorized ('{"login": "muser", "service":"system_user", "action": "add_new_user"}');
-
--- SELECT * FROM system_user_schema.action_add_new_user ('{"firstname": "Roger", "lastname": "Fitzroger", "login": "opal", "password":"spade", "changing_user_login": "muser"}');
