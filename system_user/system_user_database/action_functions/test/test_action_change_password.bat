bb3178fa66ea7bc8db7a8d893e4bf5e9750bf0e1
Jim Cantrill <jim.cantrill@dor.org>
2018-08-03 15:38:25 -0400

    update psql path

--- system_user/system_user_database/action_functions/test/test_action_change_password.bat
+++ system_user/system_user_database/action_functions/test/test_action_change_password.bat
@@ -1,6 +1,8 @@
+REM install db
+call C:\funda_components\system_user\system_user_database\system_user_test_db\install_system_user_test_db.bat
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f ..\action_change_password.sql 1> test_action_change_password.log 2>&1 
+"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d system_user_test_db  -p 5432 -f ..\action_change_password.sql 1> test_action_change_password.log 2>&1 
 
-"C:\Program Files\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres -d report_manager_database  -p 5432 -f test_action_change_password.sql  1>> test_action_change_password.log 2>>&1 
+"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -h localhost -U postgres -d system_user_test_db  -p 5432 -f test_action_change_password.sql  1>> test_action_change_password.log 2>>&1 
 
  
