b2905b4aa8db4f4d2be5bd20f789bb083df07de2
Jim Cantrill <jim.cantrill@dor.org>
2018-08-08 09:45:09 -0400

    add more tests, coverage is now 100%

--- system_user/system_user_database/action_functions/test/test_action_change_password.sql
+++ system_user/system_user_database/action_functions/test/test_action_change_password.sql
@@ -22,6 +22,37 @@ BEGIN
 END$$;
 
 
+-- calling user isn't authorized to change others password
+DO $$
+DECLARE  
+	_output_json	json;
+BEGIN	
+	RAISE NOTICE 'TEST  Calling user is not authorized to change others password; expected result: Failure';
+	RAISE NOTICE '';
+	_output_json := (SELECT * FROM system_user_schema.action_user_login ('{"login": "opal", "password":"spade"}'));
+	_output_json := (SELECT * FROM system_user_schema.action_change_password ('{"login": "jappl", "new_password":"creek", "changing_user_login":"opal"}'));
+	RAISE NOTICE 'TEST Login result_indicator = % ', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+	_output_json := (SELECT * FROM system_user_schema.action_user_logout ('{"login": "opal"}'));
+END$$;
+
+--the user whose password is  being changed isn't in the system
+DO $$
+DECLARE  
+	_output_json	json;
+BEGIN	
+	RAISE NOTICE 'TEST 3 User not in system; expected result: Failure';
+	RAISE NOTICE '';
+	_output_json := (SELECT * FROM system_user_schema.action_user_login ('{"login": "muser", "password":"master"}'));
+	_output_json := (SELECT * FROM system_user_schema.action_change_password ('{"login": "myword", "new_password":"whoamI","changing_user_login":"muser"}'));
+	RAISE NOTICE 'TEST Login result_indicator = % ', (SELECT _output_json ->> 'result_indicator')::text;
+	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
+	RAISE NOTICE '';
+	_output_json := (SELECT * FROM system_user_schema.action_user_logout ('{"login": "muser"}'));
+END$$;
+
+-- Calling user changes login
 DO $$
 DECLARE  
 	_output_json	json;
@@ -42,17 +73,26 @@ BEGIN
 	RAISE NOTICE 'TEST Login result_indicator = % ', (SELECT _output_json ->> 'result_indicator')::text;
 	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
 	RAISE NOTICE '';
+	_output_json := (SELECT * FROM system_user_schema.action_user_logout ('{"login": "muser"}'));
+	_output_json := (SELECT * FROM system_user_schema.action_user_logout ('{"login": "opal"}'));
 END$$;
  
---the user whose password is  being changed isn't in  the system
+
+
+-- user changes their own password
 DO $$
 DECLARE  
 	_output_json	json;
 BEGIN	
-	RAISE NOTICE 'TEST 3 User not in system; expected result: Failure';
+	RAISE NOTICE 'TEST  Calling user changes their own password; expected result: Success';
 	RAISE NOTICE '';
-	_output_json := (SELECT * FROM system_user_schema.action_change_password ('{"login": "myword", "new_password":"whoamI","changing_user_login":"muser"}'));
+	_output_json := (SELECT * FROM system_user_schema.action_user_login ('{"login": "opal", "password":"shovel"}'));
+	_output_json := (SELECT * FROM system_user_schema.action_change_password ('{"login": "opal", "new_password":"hoe", "changing_user_login":"opal"}'));
 	RAISE NOTICE 'TEST Login result_indicator = % ', (SELECT _output_json ->> 'result_indicator')::text;
 	RAISE NOTICE 'Message = %', (SELECT _output_json ->> 'message')::text;
 	RAISE NOTICE '';
+	_output_json := (SELECT * FROM system_user_schema.action_user_logout ('{"login": "opal"}'));
+	_output_json := (SELECT * FROM system_user_schema.action_user_login ('{"login": "opal", "password":"hoe"}'));
+	RAISE NOTICE 'TEST Login result_indicator = % ', (SELECT _output_json ->> 'result_indicator')::text;
+	_output_json := (SELECT * FROM system_user_schema.action_user_logout ('{"login": "opal"}'));
 END$$;
